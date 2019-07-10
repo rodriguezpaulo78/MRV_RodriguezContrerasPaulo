fa974f8d5e0188d45a2369e3c4226515328667b6
paulo <rodriguezpaulo78@gmail.com>
2019-07-10 00:06:14 -0500

    ProyectoFinal2

---
+++ PROYECTO_FINAL_ZOO/Assets/Scripts/ZonaLobosAudio.cs
@@ -0,0 +1,59 @@
+﻿/*
+using UnityEngine;
+using System.Collections;
+
+public class ZonaLobosAudio : MonoBehaviour
+{
+    public AudioSource tickSource;
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag == "ZonaLobos")
+        {
+            tickSource = GetComponent<AudioSource>();
+            tickSource.Play();
+        }
+        
+    }
+}
+*/
+
+/*
+using UnityEngine;
+using System.Collections;
+
+public class ZonaLobosAudio : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    public AudioClip Scored;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.tag == "ZonaLobos")
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = Scored;
+            audioSource.Play();
+        }
+    }
+}
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class ZonaLobosAudio : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag == "ZonaLobos" )
+
+        GetComponent<AudioSource>().Play();
+
+
+
+    }
+
+}
